fd0e9eadf8f37f78f5002361e0ea7de14be7af8c
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-04-23 19:32:05 +0530

    Feat: add the file stracture

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,11 @@
-import { useEffect, useState } from 'react';
-import Loader from './components/Loader';
-import Hack4maha from './Hack4maha';
-import './App.css';
+import React from "react";
 
-function App() {
-  const [showLoader, setShowLoader] = useState(true);
-  const [showHack4maha, setShowHack4maha] = useState(false);
-
-  useEffect(() => {
-    // First show loader for 4s
-    const loaderTimeout = setTimeout(() => {
-      setShowLoader(false);
-      setShowHack4maha(true);
-    }, 4000);
-
-    // Optional: hide Hack4maha and show main site after few seconds
-    const hack4mahaTimeout = setTimeout(() => {
-      setShowHack4maha(false);
-    }, 7000); // 4s loader + 3s Hack4maha
-
-    return () => {
-      clearTimeout(loaderTimeout);
-      clearTimeout(hack4mahaTimeout);
-    };
-  }, []);
-
+const App = () => {
   return (
-    <>
-      {showLoader ? (
-        <Loader />
-      ) : showHack4maha ? (
-        <Hack4maha
-          sentence="Hack4Maharashtra"
-          manualMode={false}
-          blurAmount={5}
-          borderColor="red"
-          animationDuration={2}
-          pauseBetweenAnimations={1}
-        />
-      ) : (
-        <div className="app">
-        <Hack4maha
-        sentence="Hack4Maharashtra"
-        manualMode={false}
-        blurAmount={5}
-        borderColor="red"
-        animationDuration={2}
-        pauseBetweenAnimations={1}
-      />
-
-        </div>
-      )}
-    </>
+    <div>
+      {/* <h1 className="text-amber-300">Hack 4 Maha</h1> */}
+    </div>
   );
-}
+};
 
 export default App;
